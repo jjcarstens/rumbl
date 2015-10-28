d3a4bb805e64c9603d5069a3bcdd5daa2e22c49e
Jon Carstens <jon.carstens@mx.com>
2015-10-28 09:28:34 -0600

    Fix failed password_hash save

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -19,15 +19,15 @@ defmodule Rumbl.UserController do
 	end
 
 	def create(conn, %{"user" => user_params}) do
-		changeset = User.changeset(%User{}, user_params)
+		changeset = User.registration_changeset(%User{}, user_params)
 		case Repo.insert(changeset) do
 		  {:ok, user} ->
-			conn
-			|> Rumbl.Auth.login(user)
-			|> put_flash(:info, "#{user.name} created!")
-			|> redirect(to: user_path(conn, :index))
+				conn
+				|> Rumbl.Auth.login(user)
+				|> put_flash(:info, "#{user.name} created!")
+				|> redirect(to: user_path(conn, :index))
 		  {:error, changeset} ->
-			render(conn, "new.html", changeset: changeset)
+				render(conn, "new.html", changeset: changeset)
 		end
 	end
 
