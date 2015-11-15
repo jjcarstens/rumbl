e26e60ff069df375c059f4c987ea6b80c388d9ad
Jon Carstens <jon.carstens@mx.com>
2015-11-15 14:19:03 -0700

    Chapter 6: Videos

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,6 @@
 defmodule Rumbl.UserController do
 	use Rumbl.Web, :controller
-	plug :authenticate when action in [:index, :show, :new]
+	# plug :authenticate when action in [:index, :show, :new]
 	alias Rumbl.User
 
 	def index(conn, _params) do
@@ -38,16 +38,5 @@ defmodule Rumbl.UserController do
     conn
     |> put_flash(:info, "#{user.name} deleted!")
     |> redirect(to: user_path(conn, :index))
-	end
-
-	defp authenticate(conn, _opts) do
-		if conn.assigns.current_user do
-			conn
-		else
-			conn
-			|> put_flash(:error, "You must be logged in to access that page")
-			|> redirect(to: page_path(conn, :index))
-			|> halt()
-		end
 	end
 end
